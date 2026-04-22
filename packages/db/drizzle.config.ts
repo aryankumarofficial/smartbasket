c53293991d914caa1b983542b925d7de3c7776b4
Aryan Kumar <aryanak9163@gmail.com>
2026-04-23 02:07:30 +0530

    feat: setups DB package

---
+++ packages/db/drizzle.config.ts
@@ -0,0 +1,27 @@
+import path from "path"
+import { config } from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+config({
+  path: [path.resolve(process.cwd(), "../..", ".env")],
+  override: true,
+})
+
+const databaseUrl = process.env.DATABASE_URL
+
+if (!databaseUrl) {
+  throw new Error("DATABASE URL is not set!")
+}
+
+export default defineConfig({
+  out: "./.migrations",
+  dialect: "postgresql",
+  schema: "scr/schema/index.ts",
+  migrations: {
+    prefix: "unix",
+    table: "__drizzle_migrations__",
+  },
+  dbCredentials: {
+    url: databaseUrl,
+  },
+})
